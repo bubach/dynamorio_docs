eb9a69b642260b275b0974afa649b26267e255db
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-12 23:52:48 +0000

    Deploy DynamoRIO/dynamorio_docs to github.com/DynamoRIO/dynamorio_docs.git:master

---
+++ navtreedata.js
@@ -0,0 +1,77 @@
+var NAVTREE =
+[
+  [ "DynamoRIO API", "index.html", [
+    [ "The DynamoRIO API", "index.html", "index" ],
+    [ "DynamoRIO Extensions", "page_ext.html", "page_ext" ],
+    [ "DynamoRIO-Based Tools", "page_tool.html", "page_tool" ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "DynamoRIO Extension Details", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ],
+    [ "DynamoRIO Home Page", "^http://www.dynamorio.org", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"API_BT.html",
+"dr__config_8h.html#a5e24de02bea5743b74adb6e64af19ae7aebeb651e698240eb01f5c3b7a467fdd6",
+"dr__ir__instr_8h.html#a222195624605490e7844ed3737ad3668",
+"dr__ir__instr_8h.html#ae1aac8cef8bb6ab075238e38ad6888a9",
+"dr__ir__macros__aarch64_8h.html#ad4728ea383659d1eecc80c33bfc8f796",
+"dr__ir__macros__arm_8h.html#a2593ce836e3733fd435e770119befadd",
+"dr__ir__macros__arm_8h.html#a60ec1912abeca1925f64ce3a8647cda5",
+"dr__ir__macros__arm_8h.html#a911d069b9d70a5458db8f80abba0f8ce",
+"dr__ir__macros__arm_8h.html#ac0024c2f5a135b33eaf8f0a4af40ddfb",
+"dr__ir__macros__arm_8h.html#af1bcf168286801a3e6f98af83bb39651",
+"dr__ir__macros__x86_8h.html#a1d3de2845e719fe5a3f0f890efee667a",
+"dr__ir__macros__x86_8h.html#a4262d2771db2e5b1df00abbadb5634f0",
+"dr__ir__macros__x86_8h.html#a6842f919d833955a3177bf7ccc1894b3",
+"dr__ir__macros__x86_8h.html#a8ece75ab2e7faf1f7f07754c9abc37fc",
+"dr__ir__macros__x86_8h.html#ab241c7e79b3887f1a1c6da514a02244f",
+"dr__ir__macros__x86_8h.html#adb0fcbe5a751299a4bc75038a4b7ffef",
+"dr__ir__macros__x86_8h.html#afed548c213d17520dbcd3482f16de41a",
+"dr__ir__opcodes__arm_8h.html#ac36f475ca5b446f4fde4c9b90bec77c8a3f6b5d70ea2fc6aa1ef4af2717a5b0cf",
+"dr__ir__opcodes__arm_8h.html#ac36f475ca5b446f4fde4c9b90bec77c8a83a314f1297e304a365dd8ccbde1625d",
+"dr__ir__opcodes__arm_8h.html#ac36f475ca5b446f4fde4c9b90bec77c8acc2c967c9393afbd2cc4027e09b8e918",
+"dr__ir__opcodes__x86_8h.html#a05589fbab0657f08285ebdfe93f5ec9ea0934c6704aa154bfa67e7f5090b49c82",
+"dr__ir__opcodes__x86_8h.html#a05589fbab0657f08285ebdfe93f5ec9ea31640c1d6247a9adeedd91c84784aca3",
+"dr__ir__opcodes__x86_8h.html#a05589fbab0657f08285ebdfe93f5ec9ea5c7799c9ac94b622937ca9e0b2019177",
+"dr__ir__opcodes__x86_8h.html#a05589fbab0657f08285ebdfe93f5ec9ea88c99a78ef458b222456b98b5cd99c78",
+"dr__ir__opcodes__x86_8h.html#a05589fbab0657f08285ebdfe93f5ec9eab7f6d936f8df7b60abcef5930147179a",
+"dr__ir__opcodes__x86_8h.html#a05589fbab0657f08285ebdfe93f5ec9eaea1101fd43bede301a4a0e181aa6806a",
+"dr__ir__opnd_8h.html#a16af7b253440dadd46a80a4b9fddba4da3a7d2697b7445e0839bdf69d93f81dfc",
+"dr__ir__opnd_8h.html#a68c8e3f02a0617550c6b559f28d8ad25",
+"dr__ir__utils_8h.html#a2c491ac5c40069f120b0ef512ccc7166",
+"dr__proc_8h.html#abde92db21512f07aaf05d073c43832df",
+"dr__tools_8h.html#a994e632ea892a947612b44027467ea2b",
+"functions_vars_p.html",
+"group__drcovlib.html#ga28c1e51038eae38438767c489d42499e",
+"group__drmgr.html#gaf78a2dccf0cfbd0f0e07b0820c735fe9",
+"group__drwrap.html#ga14db611e4346e695e930ca0b710b598a",
+"page_drgui.html#sec_drgui_setup",
+"struct__drsym__compound__type__t.html",
+"union__dr__zmm__t.html#a34fdc4ad61263ded5e160809fba73a05"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/pages_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['register_20management',['Register Management',['../page_drreg.html',1,'page_ext']]],
+  ['release_20notes_20for_20version_208_2e0_2e18425',['Release Notes for Version 8.0.18425',['../release_notes.html',1,'index']]]
+];
